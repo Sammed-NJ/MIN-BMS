d8ce94861c2329e824dec6e0cb74ae9a863d0254
Sammed NJ <samnj6969@gmail.com>
2021-01-06 17:46:50 +0530

    Init

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+// div
+const logBox = document.querySelector('.content-log');
+const signBox = document.querySelector('.content-sign');
+
+// btn
+
+// click btn to go to .content-sign div
+const signBtn =document.querySelector('.sign-btn');
+// click btn to go to .content-log div
+const logBtn =document.querySelector('.log-btn');
+
+
+signBtn.addEventListener('click', function() {
+
+    logBox.classList.add('hide');
+    signBox.classList.remove('hide');
+});
+
+logBtn.addEventListener('click', function() {
+
+    logBox.classList.remove('hide');
+    signBox.classList.add('hide');
+});
